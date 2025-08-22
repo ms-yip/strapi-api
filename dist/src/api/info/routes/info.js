2f3ea56d40af702e39c15fde160aeb4411944ffb
Yvonne <>
2025-08-22 08:34:22 +0800

    1

---
+++ dist/src/api/info/routes/info.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * info router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::info.info');
